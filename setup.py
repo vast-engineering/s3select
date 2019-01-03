16ad417fa586305cd639af2e0902a0d6a25161b5
Marko Bastovanovic <markob@vast.com>
2019-01-03 11:58:34 +0100

    Bump to v0.0.8

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name='s3select',
-    version='0.0.7',
+    version='0.0.8',
     scripts=['s3select'],
     author="Marko Baštovanović",
     author_email="marko.bast@gmail.com",
