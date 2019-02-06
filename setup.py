2805d2e0ddaf9c493612e69198b24257470c91fa
Marko Bastovanovic <markob@vast.com>
2019-02-06 14:59:14 +0100

    Release 0.0.12

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name='s3select',
-    version='0.0.11',
+    version='0.0.12',
     scripts=['s3select'],
     author="Marko Baštovanović",
     author_email="marko.bast@gmail.com",
